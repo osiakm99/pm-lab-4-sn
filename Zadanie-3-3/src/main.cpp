87a949418019cdd4fe1eb38eea0ae375dc937948
Maciej <maciej.osiak@student.wat.edu.pl>
2021-01-11 10:27:32 +0100

    Wykonano zadanie 3.3.1 - tekst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-void setup() {  
+void setup() {
+    lcd.begin(16, 2);
+    lcd.print("Kiszka");
 }
 
-void loop() {  
+void loop() {
+    lcd.setCursor(0, 1);
+    lcd.print(millis() / 1000);
+    delay(500);
 }
